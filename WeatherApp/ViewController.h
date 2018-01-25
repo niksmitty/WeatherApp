5b47313156b9a9b40530f971a26619502417b138
niksmitty <niksmitty@rambler.ru>
2018-01-25 15:16:50 +0500

    Beginning of work: device location and getting weather information by city.

--- WeatherApp/ViewController.h
+++ WeatherApp/ViewController.h
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "OpenWeatherMapAPIManager.h"
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <CLLocationManagerDelegate> {
+    CLLocationManager *locationManager;
+    OpenWeatherMapAPIManager *OWMAPIManager;
+}
 
 @end
-
--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LocationInfo.h"
 
 @interface ViewController ()
 
@@ -16,14 +17,62 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *apiKey = @"81d2bde991f9d4ee935b9cc996d94b9c";
+    OWMAPIManager = [[OpenWeatherMapAPIManager alloc] initWithApiKey:apiKey];
+    
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+        [locationManager requestWhenInUseAuthorization];
+    }
+    
+    [locationManager startUpdatingLocation];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)getInfoOfLocation:(CLLocation*)location completion:(void (^)(LocationInfo*))completionHandler {
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if (!error) {
+            if (placemarks && placemarks.count > 0) {
+                CLPlacemark *placemark = [placemarks objectAtIndex:0];
+                //NSString *address = [NSString stringWithFormat:@"%@ %@,%@ %@", [placemark subThoroughfare], [placemark thoroughfare], [placemark locality], [placemark administrativeArea]];
+                LocationInfo *locationInfo = [LocationInfo new];
+                locationInfo.subThoroughfare = [placemark subThoroughfare];
+                locationInfo.thoroughfare = [placemark thoroughfare];
+                locationInfo.locality = [placemark locality];
+                locationInfo.administrativeArea = [placemark administrativeArea];
+                if (completionHandler)
+                    completionHandler(locationInfo);
+            }
+        } else {
+            NSLog(@"Geocode failed with error: %@", error);
+            completionHandler(nil);
+        }
+    }];
 }
 
+#pragma mark - CLLocationManagerDelegate
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    [locationManager stopUpdatingLocation];
+    locationManager.delegate = nil;
+    locationManager = nil;
+    CLLocation *newLocation = [locations lastObject];
+    NSLog(@"NewLocation %f %f", newLocation.coordinate.latitude, newLocation.coordinate.longitude);
+    [self getInfoOfLocation:newLocation completion:^(LocationInfo *locationInfo) {
+        [OWMAPIManager getCurrentWeatherByCityname:locationInfo.locality withCompletionHandler:^(NSError *error, NSDictionary *result) {
+            if (error) {
+                NSLog(@"%@", error);
+            } else {
+                NSLog(@"%@", result);
+            }
+        }];
+    }];
+}
 
 @end
