ff1f9305304afc0b9065b5febd4a2f1260dd958b
niksmitty <niksmitty@rambler.ru>
2018-01-25 15:07:15 +0500

    Added location model class.

---
+++ WeatherApp/LocationInfo.h
@@ -0,0 +1,26 @@
+//
+//  LocationInfo.h
+//  WeatherApp
+//
+//  Created by Nikita Votyakov on 24.01.18.
+//  Copyright © 2018 Nikita Votyakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationInfo : NSObject
+
+@property (nonatomic, strong) NSString *address;
+@property (nonatomic, strong) NSString *region;
+@property (nonatomic, strong) NSString *country;
+@property (nonatomic, strong) NSString *locality;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *ocean;
+@property (nonatomic, strong) NSString *postalCode;
+@property (nonatomic, strong) NSString *subLocality;
+@property (nonatomic, strong) NSString *location;
+@property (nonatomic, strong) NSString *thoroughfare;
+@property (nonatomic, strong) NSString *subThoroughfare;
+@property (nonatomic, strong) NSString *administrativeArea;
+
+@end
---
+++ WeatherApp/LocationInfo.m
@@ -0,0 +1,13 @@
+//
+//  LocationInfo.m
+//  WeatherApp
+//
+//  Created by Nikita Votyakov on 24.01.18.
+//  Copyright © 2018 Nikita Votyakov. All rights reserved.
+//
+
+#import "LocationInfo.h"
+
+@implementation LocationInfo
+
+@end
